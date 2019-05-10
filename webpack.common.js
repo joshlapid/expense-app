240da9e878889a68664ff138d51c949c91a1d162
joshlapid <lapidjoshua@gmail.com>
2019-05-10 16:13:20 -0700

    Setup production webpack build

---
+++ webpack.common.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCSSExtract = new MiniCssExtractPlugin({
+    filename: 'style.css'
+});
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+
+        }, {
+            test: /\.s?css$/,
+            use: [
+                {
+                    loader: MiniCssExtractPlugin.loader,
+                },
+                {
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                },
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                }
+            ],
+        }]
+    },
+    plugins: [
+        MiniCSSExtract
+    ]
+};
