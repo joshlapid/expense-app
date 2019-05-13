331f75874ed6d233ad827d594b110ba89dcf19fc
joshlapid <lapidjoshua@gmail.com>
2019-05-13 13:11:14 -0700

    Setup dev dependencies and dist folder

--- webpack.common.js
+++ webpack.common.js
@@ -7,7 +7,7 @@ const MiniCSSExtract = new MiniCssExtractPlugin({
 module.exports = {
     entry: './src/app.js',
     output: {
-        path: path.join(__dirname, 'public'),
+        path: path.join(__dirname, 'public', 'dist'),
         filename: 'bundle.js'
     },
     module: {
